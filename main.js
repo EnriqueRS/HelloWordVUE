0e25571a5855aa7d8bc463753304494f20a74818
EnriqueRS <enrique.rios.santos@gmail.com>
2021-05-03 08:35:20 +0200

    computed, bootstrap added, v-on:click @click, methods

--- main.js
+++ main.js
@@ -2,9 +2,29 @@ const app = Vue.createApp({
     data(){
         return {
             tittle: 'HelloWord Vue.js',
-            amount: 200,
+            amount: 1000,
             link: 'http://www.youtube.com',
-            state: true
+            state: true,
+            services: ['Payments', 'Money Transfers', 'Bank Transfers'],
+            disable: false
+        }
+    },
+    methods:{
+        addMoney(){
+            this.amount += 100
+        },
+        decreaseMoney() {
+            if(this.amount <= 0) {
+                this.disable = true
+                alert('No money')
+                return
+            }
+            this.amount -= 100
+        }
+    },
+    computed:{
+        amountColor(){
+            return this.amount > 500 ? 'text-success' : 'text-danger'
         }
     }
 })
