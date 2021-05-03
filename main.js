0196b07b12f54671921719ddf4e16b0032298720
EnriqueRS <enrique.rios.santos@gmail.com>
2021-05-03 08:02:03 +0200

    Firsts steps

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data(){
         return {
-            tittle: 'HelloWord Vue.js'
+            tittle: 'HelloWord Vue.js',
+            amount: 200,
+            link: 'http://www.youtube.com',
+            state: true
         }
     }
 })
