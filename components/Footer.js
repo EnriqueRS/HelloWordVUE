e1c3fc49b51f632ffc26bc2d0424e108c3a765f8
EnriqueRS <enrique.rios.santos@gmail.com>
2021-05-03 09:43:11 +0200

    props

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 app.component('footer-bank',{
+    props: ['value', 'fecha'],
     template: /*html*/`
     <div class="bg-dark py-3 mt-2 text-white">
-    <h3>{{ text }}</h3>
+    <h3>{{ text }} - {{ value }}</h3>
+    <p>{{ fecha }}</p>
     </div>
     `,
     data() {
